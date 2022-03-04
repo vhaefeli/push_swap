2e0078a51e0504c53cc9b7e0a9ed682b571ca456
ricardoreves <ricardo.reves@gmail.com>
2022-03-04 01:44:29 +0100

    add libft

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 14:18:12 by vhaefeli          #+#    #+#             */
+/*   Updated: 2022/03/04 00:02:04 by rpinto-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	while (len > 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)s + len);
+		if (!len)
+			break ;
+		len--;
+	}
+	return (NULL);
+}
